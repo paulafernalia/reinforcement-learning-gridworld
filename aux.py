4254bb9f3a95d7a37e581e314b55ff386dbf5dcc
Paula <paula.fermin.cueto@gmail.com>
2018-05-30 17:43:58 +0200

    Windy girdworld

--- aux.py
+++ aux.py
@@ -22,14 +22,14 @@ class GridMap:
         return out
 
 
-def plotGrid(GridWorld):
+def plotGrid(GridWorld, figs = (8,4)):
     ''' Plot empty grid map with grass and cliff. '''
 
     cmap   = colors.ListedColormap(['pink', 'lightgray'])
     bounds = [GridWorld.cliffRwd, GridWorld.gridRwd ,0]
     norm   = colors.BoundaryNorm(bounds, cmap.N)
 
-    fig, ax = plt.subplots(figsize = (8,4))
+    fig, ax = plt.subplots(figsize = figs)
     ax.imshow(GridWorld.grid, cmap=cmap, norm=norm)
 
     # draw gridlines
@@ -48,16 +48,21 @@ def plotGrid(GridWorld):
     return([fig,ax])
 
 
-def moveAgent(state, GridObj, a):
+def moveAgent(state, GridObj, a, wind):
     ''' Get agent's next state based on current state and given action.
     If the agent tries to move outside the boundaries of the gridmap,
     # it will remain in the same position. '''
-
+    
     nextState = state.copy()
     if   a == 'UP':    nextState[1] = max(state[1] - 1, 0)
     elif a == 'RIGHT': nextState[0] = min(state[0] + 1, GridObj.w - 1)
     elif a == 'DOWN':  nextState[1] = min(state[1] + 1, GridObj.h - 1)
     else:              nextState[0] = max(state[0] - 1, 0)
+    
+    if wind != []:
+        if wind[nextState[1], nextState[0]] > 0: 
+            nextState[1] = max(nextState[1] - wind[nextState[1], nextState[0]], 0) 
+    
     return(nextState)
 
 
@@ -67,13 +72,16 @@ def selectAction(Q, actions, s, eps = 0.1):
     thresh = np.random.uniform()
 
     if thresh > eps:
-        action = actions[np.argmax(Q[:, s[1], s[0]])] # Select greedy action
+        #actionOld = actions[np.argmax(Q[:, s[1], s[0]])] # Select greedy action
+        maxQ = np.max(Q[:, s[1], s[0]])
+        argmaxQ = actions[Q[:, s[1], s[0]] == maxQ]
+        action = random.choice(argmaxQ)
     else:
         action = random.choice(actions) # Select random action
     return(action)
 
 
-def progress(acValFun, acs, eps, state, gridObj):
+def progress(acValFun, acs, eps, state, gridObj, wind=[]):
     '''Update action, next state and reward. '''
 
     # Select action for behaviour, move agent according to selected action
@@ -81,7 +89,7 @@ def progress(acValFun, acs, eps, state, gridObj):
 
     # Terminating state returns to itself with reward 0
     if state != gridObj.end:
-        nextSt = moveAgent(state, gridObj, ac)
+        nextSt = moveAgent(state, gridObj, ac, wind)
         reward = gridObj.grid[nextSt[1], nextSt[0]]
     else:
         nextSt = state.copy()
@@ -103,11 +111,18 @@ def updateOnline(Q, a, s, alph, R, a_tilde, s_tilde, mode):
     return(Q)
 
 
-def selectEpisode(plotInt, episode):
+def selectEpisode(plotInt, episode, first=True):
     ''' Determine whether this episode should be plotted. '''
 
-    cond1 = round(episode/plotInt) == episode/plotInt
-    cond2 = episode in [5,15]
+    if first == False: 
+        cond0 = episode != 0
+        cond2 = False
+    else:
+        cond0 = True
+        cond2 = episode in [5,15]
+        
+    cond1 = (round(episode/plotInt) == episode/plotInt) and cond0
+    
     return(cond1 or cond2)
 
 
